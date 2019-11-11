065699b9301e9dc50d415a60d595f60eacf28e1d
serendia95 <ichsanmedi95@gmail.com>
2019-11-11 15:25:32 +0700

    Tolong dicek ya

---
+++ Modul 01 JCDS 07/Matrix.py
@@ -0,0 +1,19 @@
+# Memutar matriks 90 derajat ke kanan
+
+# a = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+# hasil = []
+
+# for i in range(len(a)):
+#     kecil = []
+
+#     for j in range(len(a)):
+#         b = a[len(a) - 1 - j][i]
+#         kecil.append(b)
+#     hasil.append(kecil)
+
+# print(hasil)
